cc2dde45454045f0ec15fcdff17b9625b2857e4c
odiaratnakalakar-coder <odiaratnakalakar@gmail.com>
2026-09-13 20:49:57 +0530

    Refactor firebaseConfig for better readability

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,20 +9,38 @@ import { getAuth } from
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYJ_KKsS_7SNz108jnw6hpOlmegofRhJg",
-  authDomain: "utkalamart-1ec68.firebaseapp.com",
-  projectId: "utkalamart-1ec68",
-  storageBucket: "utkalamart-1ec68.firebasestorage.app",
-  messagingSenderId: "664871353747",
-  appId: "1:664871353747:web:96532fdafe78aabe7f3c66"
+
+  apiKey:
+    "AIzaSyDonHK2CtrUjAkAaro6stnJmdDUwB59JT0",
+
+  authDomain:
+    "utkalamart-1ec68.firebaseapp.com",
+
+  projectId:
+    "utkalamart-1ec68",
+
+  storageBucket:
+    "utkalamart-1ec68.firebasestorage.app",
+
+  messagingSenderId:
+    "664871353747",
+
+  appId:
+    "1:664871353747:web:96532fdafe78aabe7f3c66"
+
 };
 
 
-const app = initializeApp(firebaseConfig);
+const app =
+  initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
 
-const auth = getAuth(app);
+const db =
+  getFirestore(app);
+
+
+const auth =
+  getAuth(app);
 
 
 export {
